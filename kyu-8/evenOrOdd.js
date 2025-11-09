e6cf173698da3a8e66abb3ed2e9ecd369fbe6a16
icank smith <sammykitkat8@gmail.com>
2025-11-09 09:44:41 +0700

    add kyu-8 folder & clear my first quiz on codewars

---
+++ kyu-8/evenOrOdd.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
+
+function evenOrOdd(number) { 
+    if (i % 2 === 0){
+        return 'Even'; //E kapital
+    } else {
+        return 'Odd' //O kapital
+    }
+}
+
+console.log(evenOrOdd());
+
+//ini keterlaluan anjeng, masa beda satu huruf doang testnya salah bgsd
+//anyway this is my first codewars answer hehe
+
