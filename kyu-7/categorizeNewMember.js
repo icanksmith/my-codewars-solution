b0ee04078467a93d63e6d857848282b381bcb621
icank smith <sammykitkat8@gmail.com>
2025-11-11 13:50:41 +0700

    add comment block clean code version for categorizeNewMember.js

--- kyu-7/categorizeNewMember.js
+++ kyu-7/categorizeNewMember.js
@@ -19,3 +19,22 @@ function openOrSenior(data){
 console.log(openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]])); // ['Open', 'Senior', 'Open', 'Senior']
 console.log(openOrSenior([[3, 12],[55,1],[91, -2],[53, 23]])); // ['Open', 'Open', 'Open', 'Open']
 console.log(openOrSenior([[59, 12],[55,-1],[12, -2],[12, 12]])); // ['Senior', 'Open', 'Open', 'Open']
+
+/* versi clean GPT 
+function openOrSenior(data) {
+  const newMember = [];
+  
+  for (let i = 0; i < data.length; i++) {
+    const age = data[i][0];
+    const handicap = data[i][1];
+    
+    if (age >= 55 && handicap > 7) {
+      newMember.push('Senior');
+    } else {
+      newMember.push('Open');
+    }
+  }
+  
+  return newMember;
+}
+ */
